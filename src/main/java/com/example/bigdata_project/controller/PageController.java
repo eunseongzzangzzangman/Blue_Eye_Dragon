5ba24f37b352053f65971ffc61e1f068da139c36
ghwntksanfaldur <tmdqts55@naver.com>
2023-10-15 18:20:21 +0900

    1015 오후6시

--- src/main/java/com/example/bigdata_project/controller/PageController.java
+++ src/main/java/com/example/bigdata_project/controller/PageController.java
@@ -16,10 +16,52 @@ public class PageController {
         System.out.println("Asdfasdfasdf");
         return "url_code(경찰관수대비)";
     }
+    @GetMapping("/url_code(메인대시보드).html")
+    public String tester2(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(메인대시보드)";
+    }
+    @GetMapping("/url_code(유동인구필터링).html")
+    public String teste3r(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(유동인구필터링)";
+    }
+    @GetMapping("/url_code(인프라수).html")
+    public String tester4(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(인프라수)";
+    }
+    @GetMapping("/url_code(주점업장수).html")
+    public String tester5(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(주점업장수)";
+    }
+    @GetMapping("/url_code(퇴근시간유입인구).html")
+    public String tester6(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(퇴근시간유입인구)";
+    }
+
+    @GetMapping("/url_code(7).html")
+    public String tester7(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(7)";
+    }
+
 
     @GetMapping("/main2")
     public String showMainPage2(){
         return "main2";
+    }
+
+    @GetMapping("/main4")
+    public String showMainPage4(){
+        return "main4";
+    }
+
+    @GetMapping("/main5")
+    public String showMainPage5(){
+        return "main5";
     }
 
     @GetMapping("/page1")
