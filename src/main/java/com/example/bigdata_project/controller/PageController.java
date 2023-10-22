370a6dcba0690cb637aa45372416295f4832cb53
ghwntksanfaldur <tmdqts55@naver.com>
2023-10-22 18:20:54 +0900

    1023 오후12시

--- src/main/java/com/example/bigdata_project/controller/PageController.java
+++ src/main/java/com/example/bigdata_project/controller/PageController.java
@@ -11,42 +11,177 @@ public class PageController {
         return "main";
     }
 
-    @GetMapping("/url_code(경찰관수대비).html")
-    public String tester(){
-        System.out.println("Asdfasdfasdf");
-        return "url_code(경찰관수대비)";
-    }
+
+
     @GetMapping("/url_code(메인대시보드).html")
-    public String tester2(){
+    public String tester(){
         System.out.println("Asdfasdfasdf");
         return "url_code(메인대시보드)";
     }
-    @GetMapping("/url_code(유동인구필터링).html")
-    public String teste3r(){
+
+    // 영등포// 영등포// 영등포// 영등포// 영등포// 영등포// 영등포// 영등포// 영등포// 영등포// 영등포// 영등포
+    @GetMapping("/url_code(영등포_주점업장수).html")
+    public String tester2(){
         System.out.println("Asdfasdfasdf");
-        return "url_code(유동인구필터링)";
-    }
-    @GetMapping("/url_code(인프라수).html")
-    public String tester4(){
-        System.out.println("Asdfasdfasdf");
-        return "url_code(인프라수)";
-    }
-    @GetMapping("/url_code(주점업장수).html")
-    public String tester5(){
-        System.out.println("Asdfasdfasdf");
-        return "url_code(주점업장수)";
-    }
-    @GetMapping("/url_code(퇴근시간유입인구).html")
-    public String tester6(){
-        System.out.println("Asdfasdfasdf");
-        return "url_code(퇴근시간유입인구)";
+        return "url_code(영등포_주점업장수)";
     }
 
-    @GetMapping("/url_code(7).html")
+    @GetMapping("/url_code(영등포_인프라수).html")
+    public String tester3(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(영등포_인프라수)";
+    }
+
+    @GetMapping("/url_code(영등포_유동인구필터링).html")
+    public String tester4(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(영등포_유동인구필터링)";
+    }
+
+
+    @GetMapping("/url_code(영등포_퇴근시간유입인구).html")
+    public String tester5(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(영등포_퇴근시간유입인구)";
+
+    }
+
+    @GetMapping("/url_code(영등포_경찰관수대비).html")
+    public String tester6(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(영등포_경찰관수대비)";
+    }
+
+
+
+    //강서 //강서//강서//강서//강서//강서//강서//강서//강서//강서//강서//강서//강서//강서//강서//강서//강서//강서//강서//강서
+    @GetMapping("/url_code(강서_주점업장수).html")
     public String tester7(){
         System.out.println("Asdfasdfasdf");
-        return "url_code(7)";
+        return "url_code(강서_주점업장수)";
     }
+
+    @GetMapping("/url_code(강서_인프라수).html")
+    public String tester8(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(강서_인프라수)";
+    }
+    @GetMapping("/url_code(강서_유동인구필터링).html")
+    public String tester9(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(강서_유동인구필터링)";
+    }
+    @GetMapping("/url_code(강서_퇴근시간유입인구).html")
+    public String tester10(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(강서_퇴근시간유입인구)";
+    }
+    @GetMapping("/url_code(강서_경찰관수대비).html")
+    public String tester11(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(강서_경찰관수대비)";
+    }
+
+    //강남//강남//강남//강남//강남//강남//강남//강남//강남//강남//강남//강남//강남//강남//강남//강남//강남//강남//강남//강남//강남//강남//강남//강남//강남//강남
+
+    @GetMapping("/url_code(강남_주점업장수).html")
+    public String tester12(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(강남_주점업장수)";
+    }
+
+    @GetMapping("/url_code(강남_인프라수).html")
+    public String tester13(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(강남_인프라수)";
+    }
+    @GetMapping("/url_code(강남_유동인구필터링).html")
+    public String tester14(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(강남_유동인구필터링)";
+    }
+    @GetMapping("/url_code(강남_퇴근시간유입인구).html")
+    public String tester15(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(강남_퇴근시간유입인구)";
+    }
+    @GetMapping("/url_code(강남_경찰관수대비).html")
+    public String tester16(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(강남_경찰관수대비)";
+    }
+
+
+
+    //강동 //강동 //강동 //강동 //강동//강동//강동 ㅍ //강동 //강동 //강동//강동 //강동
+    @GetMapping("/url_code(강동_주점업장수).html")
+    public String tester17(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(강동_주점업장수)";
+    }
+
+    @GetMapping("/url_code(강동_인프라수).html")
+    public String tester18(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(강동_인프라수)";
+    }
+    @GetMapping("/url_code(강동_유동인구필터링).html")
+    public String tester19(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(강동_유동인구필터링)";
+    }
+    @GetMapping("/url_code(강동_퇴근시간유입인구).html")
+    public String tester20(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(강동_퇴근시간유입인구)";
+    }
+    @GetMapping("/url_code(강동_경찰관수대비).html")
+    public String tester21(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(강동_경찰관수대비)";
+    }
+
+
+    //예측 지도 //예측 지도 //예측 지도 //예측 지도 //예측 지도 //예측 지도 //예측 지도 //예측 지도
+    @GetMapping("/url_code(지도_서울전체).html")
+    public String tester22(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(지도_서울전체)";
+    }
+
+
+    @GetMapping("/url_code(지도_영등포).html")
+    public String tester23(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(지도_영등포)";
+    }
+
+    @GetMapping("/url_code(지도_강서).html")
+    public String tester24(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(지도_강서)";
+    }
+
+    @GetMapping("/url_code(지도_강남).html")
+    public String tester25(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(지도_강남)";
+    }
+
+    @GetMapping("/url_code(지도_강동).html")
+    public String tester26(){
+        System.out.println("Asdfasdfasdf");
+        return "url_code(지도_강동)";
+    }
+
+
+
+
+
+
+
+
+
 
 
     @GetMapping("/main2")
